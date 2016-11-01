f6adf101c142ef752a4126593e6b472298adf05b
Chris Holman <squid@sirsquidness.com>
2016-11-01 15:48:37 +1100

    add more keys

--- attributes/users.rb
+++ attributes/users.rb
@@ -9,11 +9,11 @@ default['respawn']['users'] = {
     "groups" => [sudo_group, "docker"]
   },
   "tacticus" => {
-    "key" => "",
+    "key" => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+ggNbKjPXVggdJAgrtksBdZtYzxZWJ+hKAAQgo5GA7 kca@Kevins-MacBook-Pro.local",
     "groups" => [sudo_group, "docker"]
     },
   "bonbon" => {
-    "key" => "",
+    "key" => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQjMPJsLqIv8AyzdrqaHTn2p+lElo0ti+YjB/mdTroKvDK4lF7YbsdI5Vy/YTUI5LXy+kigpUDkRXmg2MuP2pOBGxPJbVy02MIrBxVN52qVwpzdnyPwUALss0bE+BTCoCD//PeyaCvw6S2a5UNS9rQCqnnyrLwyyovRMiHxdEKmuG6o58mCBxaVA05Btjo21EeA0ovwj4vmVny7IIuXyIqpM2E1Vp49VL+2B2v6pgGbOxcj58+TbPRdkzX1pxjFxRMbLxPZBdqU0TXtlkypxPh8jx0JpMeiKYRGBxV5E7t51QEM9wa7RpTEBSXympNwRZHpnsinlWl6FeF2a2QV9R5 danielsalamy@me.com",
     "groups" => [sudo_group, "docker"]
   },
   "priorax" => {
@@ -30,6 +30,10 @@ default['respawn']['users'] = {
   },
   "zardoz" => {
     "key" => "",
+    "groups" => [sudo_group, "docker"]
+  },
+  "kelso" => {
+    "key" => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs+kibpkwP3ow5LRY33bFhuxOs22CPYDi5TY7I+8veMBAPxy2bNKdZMgvrfPOVJ1nS4U6ggKhdkDaCJBZ0m/d093GtVW2iX/t8YU6GMfwyHst6KIoAcMQrsupUeLXHQX6Vj1QiRSMpWTfOQO0CKK/KH/nnf4Jz2/2gPADNe1tHghBV4hXd71I1ur6LFqRAktFy9OdsAMFUE4kmPkkb1LchALX8ldpWmtdfljwAg0Z2t1W8V+6cl4Fq3Mz6YeQ9SRD76mwFTLxhLxbrdU3z8z9zKKpES3zQKyzpGJzt1dJqz+yEG+OhMHF+v98q/wrlKk6NwAnk3sfgbAuRdgvx5r1t Kelso@Discordia",
     "groups" => [sudo_group, "docker"]
   }
 
